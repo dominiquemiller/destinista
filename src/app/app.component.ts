cdb667b4a09898fb4251d7022657a8e26bc1b41e
Dominique Miller <dominique.p.miller@gmail.com>
2017-07-13 23:02:11 -0400

    Gamer-info component Create formGroup and controls for sign in. Add basic styling to sign-in and add bg image.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Destinista!';
+
+  getGamer(gamer) {
+    console.log(gamer);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
+import { GamerInfoComponent } from './components/gamer-info/gamer-info.component';
+
+// Services
+import { GamerTagService } from './services/gamer-tag.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamerInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    GamerTagService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
