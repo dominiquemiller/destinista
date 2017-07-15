b4e7b3935962997c74bf058429866a6b97b4feed
Dominique Miller <dominique.p.miller@gmail.com>
2017-07-14 22:43:27 -0400

    GamerInfoService. Connect to API from emit event of sign in form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { GamerInfoService } from './services/gamer-info/gamer-info.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Destinista!';
+
+  constructor(private service: GamerInfoService ) {};
+
+  ngOnInit() {}
 
   getGamer(gamer) {
     console.log(gamer);
+    this.service.get(gamer).subscribe( data => console.log(data));
   }
+
 }
