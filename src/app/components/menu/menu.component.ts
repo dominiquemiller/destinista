b783c5f7c1f8dd852d002527b13c5abd083b361c
Dominique Miller <dominique.p.miller@gmail.com>
2017-07-21 23:20:30 -0400

    update spinnerservice spec

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -11,7 +11,6 @@ import { CharClass } from '../../models/char-class.enum';
 })
 
 export class MenuComponent implements OnInit {
-    banners: Array<string> = [];
     charClass;
     summary;
     characters;
